fb3d0cf0d23c1dbb9492970f59f0309c087a7ec7
qtgeo1248 <gzhou00@stuy.edu>
2020-02-02 13:33:32 -0500

    made it faster

--- Picmaker.java
+++ Picmaker.java
@@ -3,23 +3,26 @@ import java.lang.Math;
 
 public class Picmaker {
     public static boolean isElement(int a, int b) { //a + bi
+        double x = 0.0;
+        double y = 0.0;
+        for (int i = 0; i < 1000; i++) {
+
+        }
         return true;
     }
 
     public static void main(String[] args) {
         try {
             FileWriter writer = new FileWriter("mandel.ppm");
-            String heading = "P3 500 500 255\n";
+            String current = "P3 500 500 255\n";
+            writer.write(current);
             for (int i = 250; i > -250; i--) { //x coord
                 for (int j = 250; j > -250; j--) { //y coord
-                    heading += (Math.abs(j)) % 255 + " ";
-                    heading += "0 0 ";
-                }
-                if (i != -499) {
-                    heading += "\n";
+                    current = Math.abs(j) % 255 + " ";
+                    current += "0 0 ";
+                    writer.write(current);
                 }
             }
-            writer.write(heading);
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
